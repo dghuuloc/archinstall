a6e3840864cc82baef2abf64bebc586e171f4bca
dghuuloc <87122072+dghuuloc@users.noreply.github.com>
2022-02-26 10:51:26 +0700

    Update arch-install-base.sh

--- arch-install-base.sh
+++ arch-install-base.sh
@@ -17,3 +17,12 @@ echo "--------------------------------------"
 pacman -S --needed --noconfirm grub grub-btrfs efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Install packages
+pacman -S --needed networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers xdg-user-dirs xdg-utils inetutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion reflector acpi git flatpak acpid terminus-font
+
+systemctl enable NetworkManager
+# systemctl enable bluetooth
+# systemctl enable cups
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
