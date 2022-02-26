6b34229de4c666209d966f9919ef8615f02a2375
dghuuloc <87122072+dghuuloc@users.noreply.github.com>
2022-02-26 10:46:10 +0700

    Update arch-install-base.sh

--- arch-install-base.sh
+++ arch-install-base.sh
@@ -2,3 +2,18 @@
 
 # Arch Linux Fast Installer
 
+echo "--------------------------------------"
+echo "-- Arch Install on Main Drive       --"
+echo "--------------------------------------"
+
+pacstrap /mnt base linux linux-firmware git nano intel-ucode btrfs-progs --noconfirm --needed
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+
+echo "--------------------------------------"
+echo "-- Bootloader Systemd Installation  --"
+echo "--------------------------------------"
+pacman -S --needed --noconfirm grub grub-btrfs efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
